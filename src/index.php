53c4d43ac41edeca30656ec823bf1e221aa93d35
chandimal-kumara <kumara.8375@gmail.com>
2026-02-24 14:36:02 +0530

    CICD Test 2

--- src/index.php
+++ src/index.php
@@ -29,7 +29,7 @@
                     <div class="col-12 section_1 py-3 p-3">
                         <div class="row">
                             <div class="col-12 col-md-6 mt-5 text-center m-auto">
-                                <h1 class="title_01">Your Trusted Calculator for </br> Daily Finances - Test</h1>
+                                <h1 class="title_01">Your Trusted Calculator for </br> Daily Finances</h1>
                                 <p class="fs-4">Accurate, easy-to-use financial tailored for Sri Lanka.</br> Simplify your daily finances tasks with DailyCalc LK.</p>
                                 <div class="row">
                                     <div class="col-12 d-flex justify-content-center gap-3">
